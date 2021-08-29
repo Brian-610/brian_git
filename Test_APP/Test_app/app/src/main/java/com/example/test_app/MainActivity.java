7ed4a030a8de9563022bd2dd496a33e8e518bcd6
Brian_H <610998054@qq.com>
2021-08-29 16:22:04 +0800

    APP首次提交

---
+++ Test_APP/Test_app/app/src/main/java/com/example/test_app/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.test_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+/*        Button btn1 = (Button) findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent是一种运行时绑定（run-time binding）机制，它能在程序运行过程中连接两个不同的组件。
+                //在存放资源代码的文件夹下下，
+                Intent i = new Intent(MainActivity.this, SecondActivity.class);
+                //启动
+                startActivity(i);
+            }
+        });
+ */
+
+        public void Button (View v) {
+            Intent i = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(i);
+        }
+
+
+
+
+}
---
+++ Test_APP/Test_app/app/src/main/java/com/example/test_app/SecondActivity.java
@@ -0,0 +1,23 @@
+package com.example.test_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+            public void first (View v) {
+
+                Intent i = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+}
